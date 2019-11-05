bc1c88603b3d3c887499ac99f63d668ac36453bb
phanimadiraju19 <phanimadiraju19@gmail.com>
2019-11-05 11:57:09 +0530

    Home page demo content added

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return (
-            <div>Home Page content goes here...</div>
+            <div><h1>Home Page content goes here...</h1>
+            <h2>Home Page content goes here...</h2>
+            <h3>Home Page content goes here...</h3></div>
         )
 
     }
